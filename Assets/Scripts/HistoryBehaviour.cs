0182ea23c83931d4443fab7f653bb912936f3436
cbellom <cbellom@unal.edu.co>
2015-05-16 18:50:11 -0500

    dynamic creation of transactions

---
+++ Assets/Scripts/HistoryBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HistoryBehaviour : MonoBehaviour {
+
+	public GameObject panelPrefabTransaction;
+	public GameObject listTransaction;
+	public UserDataBehaviour userDataBehaviour;
+
+	public void UpdateListTransaction(){
+		List<TransactionData> transactions = userDataBehaviour.data.transactions;
+		foreach(TransactionData transaction in transactions){
+			DrawTransaction(transaction);
+		}
+	}
+
+	private void DrawTransaction (TransactionData transaction){
+		GameObject newItem = Instantiate(panelPrefabTransaction) as GameObject;
+		newItem.transform.FindChild("Image").GetComponent<Image>().sprite = null;
+		newItem.transform.FindChild("Text").GetComponent<Text>().text = transaction.reference +"\n "+ transaction.value;
+		newItem.transform.SetParent(listTransaction.gameObject.transform, false);	
+	}
+}
