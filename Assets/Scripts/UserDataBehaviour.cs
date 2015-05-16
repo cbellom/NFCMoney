2309c1e1b4ae150ca57bc34acef8a23781c6f8b9
cbellom <cbellom@unal.edu.co>
2015-05-16 17:59:26 -0500

    Fix json and creation of user data behaviour

---
+++ Assets/Scripts/UserDataBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserDataBehaviour : MonoBehaviour {	
+	public TextAsset json;
+	public UserData data = new UserData();
+
+	void Awake(){
+		ReadDataFromJson ();
+	}
+
+	private void ReadDataFromJson (){
+		JsonUserParser parser = new JsonUserParser();
+		data = new UserData ();
+		parser.JSONString = json.text;
+		data = parser.Data;
+	}
+}
