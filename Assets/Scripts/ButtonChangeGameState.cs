26b5529a9cfc043deaff100e7c9f31466761a13b
cbellom <cbellom@unal.edu.co>
2015-05-16 14:54:34 -0500

    buttons to navigate in the scene

---
+++ Assets/Scripts/ButtonChangeGameState.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonChangeGameState : MonoBehaviour {
+
+	public GameStateBehaviour gameState;
+	public GameState targetState;
+	
+	public void ChangeGameState(){
+		gameState.GameState = targetState;
+	}
+
+}
