a7c667e52164bff7d96dee0f259331b043c7769e
cbellom <cbellom@unal.edu.co>
2015-05-17 12:33:08 -0500

    show icon by type of service

--- Assets/Scripts/HistoryBehaviour.cs
+++ Assets/Scripts/HistoryBehaviour.cs
@@ -8,6 +8,10 @@ public class HistoryBehaviour : MonoBehaviour {
 	public GameObject panelPrefabTransaction;
 	public GameObject listTransaction;
 	public UserDataBehaviour userDataBehaviour;
+	public Sprite social;
+	public Sprite food;
+	public Sprite transport;	
+	public Sprite defaultIcon;
 
 	public List<Color> colorByTransactionState;
 
@@ -27,7 +31,7 @@ public class HistoryBehaviour : MonoBehaviour {
 
 	private void DrawTransaction (TransactionData transaction){
 		GameObject newItem = Instantiate(panelPrefabTransaction) as GameObject;
-		newItem.transform.FindChild("Image").GetComponent<Image>().sprite = null;
+		newItem.transform.FindChild("Image").GetComponent<Image>().sprite = getImageByTransactionType(transaction.typeService);
 		newItem.transform.FindChild ("Day").GetComponent<Text> ().text = transaction.day;
 		newItem.transform.FindChild ("Reference").GetComponent<Text> ().text = "Reference: " + transaction.reference;
 		newItem.transform.FindChild ("Value").GetComponent<Text> ().text = "$ " +transaction.value.ToString();
@@ -47,5 +51,19 @@ public class HistoryBehaviour : MonoBehaviour {
 			color = colorByTransactionState[3];
 		}
 		return color;
+	}
+
+	private Sprite getImageByTransactionType(string type){
+		Sprite img;
+		if (type == "social") {
+			img = social;
+		} else if (type == "food") {
+			img = food;
+		} else if (type == "transport") {
+			img = transport;
+		} else {
+			img = defaultIcon;
+		}
+		return img;
 	}
 }
