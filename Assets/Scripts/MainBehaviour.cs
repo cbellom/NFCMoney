9a91a5e5696226a909cb694a4c87191e88c62920
Dayana Alejo <dayanalejo@gmail.com>
2015-05-17 02:48:56 -0500

    generaterechange and main
    validate
    Reload total

---
+++ Assets/Scripts/MainBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class MainBehaviour : MonoBehaviour {
+
+	public UserDataBehaviour userDataBehaviour;
+	public Text MoneyPanel;
+	// Use this for initialization
+	public void Start () {
+		puttotaluser ();
+	}
+
+	// Update is called once per frame
+	public void Update () {
+		puttotaluser ();
+	}
+
+	public void puttotaluser()
+	{
+
+		userDataBehaviour.Load ();
+		UserData userdata = UserPersistence.userData;
+		MoneyPanel.text=System.Convert.ToString(userdata.amount);
+	}
+
+}
